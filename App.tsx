23562e70171fa28e6a027258f6238f149a3c9c8f
纪南 <jinan5694@163.com>
2024-01-01 21:49:44 +0800

    migration paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Button, Appbar } from "react-native-paper";
+
+const MyAppBar = () => {
+  const _goBack = () => console.log('Went back');
+
+  const _handleSearch = () => console.log('Searching');
+
+  const _handleMore = () => console.log('Shown more');
+
+  return (
+    <Appbar.Header>
+      <Appbar.BackAction onPress={_goBack} />
+      <Appbar.Content title="Title" />
+      <Appbar.Action icon="magnify" onPress={_handleSearch} />
+      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+    </Appbar.Header>
+  );
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <MyAppBar></MyAppBar>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
