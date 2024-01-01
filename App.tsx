e6e2d375286563fcbbebb939338981183cfc57c7
纪南 <jinan5694@163.com>
2024-01-01 22:29:42 +0800

    home to detail

--- App.tsx
+++ App.tsx
@@ -1,45 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider, Button, Appbar } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { NativeStackNavigationOptions } from '@react-navigation/native-stack'
 
-const MyAppBar = () => {
-  const _goBack = () => console.log('Went back');
 
-  const _handleSearch = () => console.log('Searching');
-
-  const _handleMore = () => console.log('Shown more');
-
+// interface ScreenProps {
+//   navigation: Na
+// }
+function HomeScreen({ navigation }: any) {
   return (
-    <Appbar.Header>
-      <Appbar.BackAction onPress={_goBack} />
-      <Appbar.Content title="Title" />
-      <Appbar.Action icon="magnify" onPress={_handleSearch} />
-      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
-    </Appbar.Header>
-  );
-};
-
-
-export default function App() {
-  return (
-    <PaperProvider>
-      <MyAppBar></MyAppBar>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      </View>
-    </PaperProvider>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button mode="contained" onPress={() => navigation.navigate('Details')}>
+        Go to Details
+      </Button>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const screenOptions: NativeStackNavigationOptions = {
+    headerBackVisible: true,
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' screenOptions={screenOptions}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview'}} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+
+// const MyAppBar = () => {
+//   const _goBack = () => console.log('Went back');
+
+//   const _handleSearch = () => console.log('Searching');
+
+//   const _handleMore = () => console.log('Shown more');
+
+//   return (
+//     <Appbar.Header>
+//       <Appbar.BackAction onPress={_goBack} />
+//       <Appbar.Content title="Title" />
+//       <Appbar.Action icon="magnify" onPress={_handleSearch} />
+//       <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+//     </Appbar.Header>
+//   );
+// };
+
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <PaperProvider>
+//         <MyAppBar></MyAppBar>
+//         <View style={styles.container}>
+//           <Text>Open up App.tsx to start working on your app!</Text>
+//           <StatusBar style="auto" />
+//           <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+//             Press me
+//           </Button>
+//         </View>
+//       </PaperProvider>
+//     </NavigationContainer>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
