20efd3d439548eb8b567d2df8b35ec84a4391d7e
纪南 <jinan5694@163.com>
2024-01-01 23:54:26 +0800

    param list types

--- App.tsx
+++ App.tsx
@@ -3,32 +3,50 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider, Button, Appbar } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import type { NativeStackNavigationOptions } from '@react-navigation/native-stack'
+import type { NativeStackNavigationOptions, NativeStackScreenProps } from '@react-navigation/native-stack'
 
 
-// interface ScreenProps {
-//   navigation: Na
-// }
-function HomeScreen({ navigation }: any) {
+type RootStackParamList = {
+  Home: undefined
+  Details: { id: string, visible?: boolean }
+}
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+function HomeScreen({ navigation }: HomeScreenProps) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button mode="contained" onPress={() => navigation.navigate('Details')}>
+      <Button mode="contained" onPress={() => navigation.navigate('Details', { id: '123', visible: false })}>
         Go to Details
       </Button>
     </View>
   );
 }
 
-function DetailsScreen() {
+type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>
+
+function DetailsScreen({ route, navigation }: DetailsScreenProps) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>{route.params.id}</Text>
+      <Button mode="contained" onPress={() => navigation.push('Details', { id: '345', visible: true })}>
+        Go to Details ... again
+      </Button>
+      <Button mode="outlined" onPress={() => navigation.goBack()} style={{ marginTop: 10 }}>
+        Go back
+      </Button>
+      <Button mode="outlined" onPress={() => navigation.navigate('Home')} style={{ marginTop: 10 }}>
+        Go to Home
+      </Button>
+      <View style={{ marginTop: 10 }}>
+        <Text>123</Text>
+      </View>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
   const screenOptions: NativeStackNavigationOptions = {
@@ -37,8 +55,17 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home' screenOptions={screenOptions}>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview'}} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ 
+          title: 'Overview',
+          headerStyle: {
+            backgroundColor: '#f4511e'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}  />
+        <Stack.Screen name="Details" component={DetailsScreen} initialParams={{ id: 'qwer' }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ README.md
@@ -0,0 +1,6 @@
+# Navigation
+
+navigation.navigate - 仅打开一次
+navigation.push - 打开新页面
+
+路由传参可序列化的json，尽可能简单
